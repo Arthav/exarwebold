e19bbbc8d4d072e7743d31e3f9a561fd27966522
Arthav <christianbonafena7@gmail.com>
2019-01-24 19:30:47 +0700

    agama mudun

--- resources/views/human/agen/show.blade.php
+++ resources/views/human/agen/show.blade.php
@@ -49,7 +49,7 @@ Data Agen
           
           <!-- isi tabs data agen -->
           <div id="Data_pribadi" class="w3-container tabsdkaryawan" style="display:none">
-          <h2>Data agen: {{$user->name}}</h2>
+          <h2>Data agen: {{$agen->name}}</h2>
 
             <!-- Awal padding -->
             <div class="w3-row-padding">
@@ -115,12 +115,12 @@ Data Agen
                 </div>
 
                 <!--awal form HAPUS agen-->
-                <form class="w3-container" action="{{ route('Human.Agen.Hapus',['id' => $user->id]) }}" method="post">
+                <form class="w3-container" action="{{ route('Human.Agen.Hapus',['id' => $agen->id]) }}" method="post">
                   <div class="w3-section">
                     
                     <!-- POP UP DELETE -->
                     <div style="color:black">
-                      <h3>Apakah anda yakin untuk menghapus data agen {{ $user->nama }}?</h3>
+                      <h3>Apakah anda yakin untuk menghapus data agen {{ $agen->nama }}?</h3>
                     </div>
                     <input class="w3-button w3-block w3-red w3-section w3-padding" type="submit" value="Delete"></input>
                     {{ csrf_field() }}
@@ -150,56 +150,56 @@ Data Agen
 
                 <!-- POP UP -->
                 <!--Awal Form Edit Data Pribadi Karyawwan-->
-                <form class="w3-container" action="{{ route('Human.Agen.Edit', ['id' => $user->id]) }}" method="post">
+                <form class="w3-container" action="{{ route('Human.Agen.Ubah', ['id' => $agen->id]) }}" method="post">
                   <div class="w3-section">
 
                     <!-- NAMA -->
                     <div style="color:black">
                       <label><b>Nama</b></label>
                     </div>
-                    <input class="w3-input w3-border w3-margin-bottom" type="text" value="{{ $user->nama}}" name="nama" >
+                    <input class="w3-input w3-border w3-margin-bottom" type="text" value="{{ $agen->nama}}" name="nama" >
 
                      <!-- Jabatan -->
                      <div style="color:black">
-                        <label><b>Tanggal Lahir</b></label>
+                        <label><b>Jabatan</b></label>
                      </div>
-                     <input class="w3-input w3-border" type="date" value="{{ $user->email }}" name="email" >
+                     <input class="w3-input w3-border" type="text" value="{{ $agen->email }}" name="email" >
 
                     <!-- Email -->
                     <div style="color:black">
                       <label><b>Tanggal Lahir</b></label>
                     </div>
-                    <input class="w3-input w3-border" type="date" value="{{ $user->email }}" name="email" >
+                    <input class="w3-input w3-border" type="text" value="{{ $agen->email }}" name="email" >
 
                      <!-- Alamat -->
                      <div style="color:black">
                         <label><b>Alamat</b></label>
                      </div>
-                     <input class="w3-input w3-border" type="date" value="{{ $user->alamat }}" name="alamat" >
+                     <input class="w3-input w3-border" type="text" value="{{ $agen->alamat }}" name="alamat" >
 
                      <!-- NO TELEPON -->
                     <div style="color:black">
                       <label><b>No. Telepon 1</b></label>
                     </div>
-                    <input class="w3-input w3-border" type="text" name="telp1" value="{{$user->telp1}}">
+                    <input class="w3-input w3-border" type="text" name="telp1" value="{{$agen->telp1}}">
                     <br>
                     <div style="color:black">
                       <label><b>No. Telepon 2</b></label>
                     </div>
-                    <input class="w3-input w3-border" type="text"  name="telp2" value="{{$user->telp2}}">
+                    <input class="w3-input w3-border" type="text"  name="telp2" value="{{$agen->telp2}}">
 
                     <!-- NIK -->
                     <div style="color:black">
                       <label><b>NIK</b></label>
                     </div>
-                    <input class="w3-input w3-border" type="text" name="nik" value="{{ $user->nik }}">
+                    <input class="w3-input w3-border" type="text" name="nik" value="{{ $agen->nik }}">
                     <br>
 
                     <!-- NPWP -->
                     <div style="color:black">
                       <label><b>NPWP</b></label>
                     </div>
-                    <input class="w3-input w3-border" type="text" name="npwp" value="{{ $user->npwp }}">
+                    <input class="w3-input w3-border" type="text" name="npwp" value="{{ $agen->npwp }}">
                     <br>
 
 
@@ -209,11 +209,11 @@ Data Agen
                       <label><b>Jenis Kelamin</b></label>
                     </div>
                     <select class="w3-select w3-border" name="jeniskelamin">
-                      @if (empty($user->jeniskelamin))
+                      @if (empty($agen->jeniskelamin))
                         <option value="" disabled selected>Jenis Kelamin</option>
                       @endif
-                      <option @if ($user->jenis_kelamin == 'Wanita') selected @endif value="Perempuan">Wanita</option>
-                      <option @if ($user->jenis_kelamin == 'Pria') selected @endif value="Laki-laki">Pria</option>
+                      <option @if ($agen->jenis_kelamin == 'Wanita') selected @endif value="Perempuan">Wanita</option>
+                      <option @if ($agen->jenis_kelamin == 'Pria') selected @endif value="Laki-laki">Pria</option>
                     </select>
                     </p>
 
@@ -224,15 +224,15 @@ Data Agen
                       <label><b>Agama</b></label>
                     </div>
                     <select class="w3-select w3-border" name="agama">
-                      @if (empty($user->agama))
+                      @if (empty($agen->agama))
                         <option value="" disabled selected>Agama</option>
                       @endif
-                      <option @if ($user->agama == 'Islam') selected @endif value="Islam">Islam</option>
-                      <option @if ($user->agama == 'Kristen') selected @endif value="Kristen">Kristen</option>
-                      <option @if ($user->agama == 'Katholik') selected @endif value="Katholik">Katholik</option>
-                      <option @if ($user->agama == 'Hindu') selected @endif value="Hindu">Hindu</option>
-                      <option @if ($user->agama == 'Buddha') selected @endif value="Buddha">Budha</option>
-                      <option @if ($user->agama == 'Lainnya') selected @endif value="Lainnya">Lainnya</option>
+                      <option @if ($agen->agama == 'Islam') selected @endif value="Islam">Islam</option>
+                      <option @if ($agen->agama == 'Kristen') selected @endif value="Kristen">Kristen</option>
+                      <option @if ($agen->agama == 'Katholik') selected @endif value="Katholik">Katholik</option>
+                      <option @if ($agen->agama == 'Hindu') selected @endif value="Hindu">Hindu</option>
+                      <option @if ($agen->agama == 'Buddha') selected @endif value="Buddha">Budha</option>
+                      <option @if ($agen->agama == 'Lainnya') selected @endif value="Lainnya">Lainnya</option>
                     </select>
                     </p>
 
